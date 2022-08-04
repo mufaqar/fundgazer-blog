f31385f9ab859d55fc532b09a1cfb9b16b1a6969
Mufaqar Islam <mufaqar@gmail.com>
2022-08-04 19:38:21 +0500

    updated code

--- pages/disclaimer.js
+++ pages/disclaimer.js
@@ -1,28 +1,99 @@
-import InnerPAgeBanner from "./components/innerPAgeBanner";
+import InnerPAgeBanner from '../components/innerPAgeBanner';
 
 export default function Disclaimer() {
-    return (
-        <>
-            <InnerPAgeBanner />
+  return (
+    <>
+      <InnerPAgeBanner />
 
-            {/* Content Section End*/}
-            <section>
-                <div className='container md:w-[1020px] mx-auto py-20'>
-                    <div className="p-4">
-                        <h3 className='md:text-5xl text-3xl font-interBold font-bold text-skin-primary mb-10'>Disclaimer</h3>
-                        
-                        <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">The content odfgsdgff this website has been prepared by contributions from people from the Investments community.</p>
-                        <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">The content on this website created solely for educational purposes. It is NOT meant to be fiduciary financial advice. When in doubt, a legally registered fiduciary financial advisor may be contacted. In India, a fee-only financial advisor registered with Securities and Exchange Board of India (SEBI) may be contacted.</p>
-                        <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">The authors of this website may or may not hold positions, either long or short, in any mutual fund, ETF, stock, bond, or any another investment instrument or securities mentioned on our website. These positions may or may not change in the future without any notification whatsoever. The act of an investment instrument being mentioned on our website does NOT constitute a recommendation or endorsement for or against investment in that instrument.</p>
-                        <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">Any and all numerical calculations made on our website in any form may be incorrect and subject to changes without any notification whatsoever. India Investments community and / or the authors of content on this website, cannot be held liable in any form or manner if anyone incurs financial losses in the securities markets based on information, either directly or indirectly, from our website.</p>
-                        <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">The content on this website may include material, including but not limited to excerpts, images, and videos, which has been modified with respect to the original source material. Such modifications have been made only for the purposes of illustration, education, and aesthetics. The original source material should be considered as the definitive source of truth.</p>
-                        <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">The content on this website may include recommendations for certain software, apps, platforms, portals, services, and websites. The authors from India Investments may or may not hold positions, either long or short, in the respective companies who own or invest in these software , apps, platforms, portals, services, and websites. These positions may or may not change in the future without any notification whatsoever. The content on this website might also recommend methods about how to use these software and websites. These methods may or may not be in compliance with the terms of service of the software, apps, platforms, portals, services, and website. India Investments cannot be held liable for any damages or loss anyone suffers because of using the methods shown in our content. The user is advised to read the terms and conditions of the software and websites they use before following the methods and recommendations mentioned in our content.</p>
-                        <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">The content on this website may include external 3rd party hyperlinks which we do not own, control, or recommend in any form or manner. Although we have included web archive links for these 3rd party hyperlinks wherever possible, the content in the original 3rd party hyperlink may or may not be the same as it was when it was mentioned in our website. India Investments does not bear any responsibility whatsoever about the content on these 3rd party hyperlinks and it cannot be held liable for damages or loss anyone suffers as a result of using these 3rd party hyperlinks.</p>
-                        <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">The content on this website is provided “as is”, without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and non-infringement. In no event shall India Investments and its contributors be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the content on this website or the use or other dealings in the content on this website.</p>
-                    </div>
-                </div>
-            </section>
-            {/* Content Section End*/}
-        </>
-    )
+      {/* Content Section End*/}
+      <section>
+        <div className="container md:w-[1020px] mx-auto py-20">
+          <div className="p-4">
+            <h3 className="md:text-5xl text-3xl font-interBold font-bold text-skin-primary mb-10">
+              Disclaimer
+            </h3>
+
+            <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">
+              The content odfgsdgff this website has been prepared by
+              contributions from people from the Investments community.
+            </p>
+            <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">
+              The content on this website created solely for educational
+              purposes. It is NOT meant to be fiduciary financial advice. When
+              in doubt, a legally registered fiduciary financial advisor may be
+              contacted. In India, a fee-only financial advisor registered with
+              Securities and Exchange Board of India (SEBI) may be contacted.
+            </p>
+            <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">
+              The authors of this website may or may not hold positions, either
+              long or short, in any mutual fund, ETF, stock, bond, or any
+              another investment instrument or securities mentioned on our
+              website. These positions may or may not change in the future
+              without any notification whatsoever. The act of an investment
+              instrument being mentioned on our website does NOT constitute a
+              recommendation or endorsement for or against investment in that
+              instrument.
+            </p>
+            <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">
+              Any and all numerical calculations made on our website in any form
+              may be incorrect and subject to changes without any notification
+              whatsoever. India Investments community and / or the authors of
+              content on this website, cannot be held liable in any form or
+              manner if anyone incurs financial losses in the securities markets
+              based on information, either directly or indirectly, from our
+              website.
+            </p>
+            <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">
+              The content on this website may include material, including but
+              not limited to excerpts, images, and videos, which has been
+              modified with respect to the original source material. Such
+              modifications have been made only for the purposes of
+              illustration, education, and aesthetics. The original source
+              material should be considered as the definitive source of truth.
+            </p>
+            <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">
+              The content on this website may include recommendations for
+              certain software, apps, platforms, portals, services, and
+              websites. The authors from India Investments may or may not hold
+              positions, either long or short, in the respective companies who
+              own or invest in these software , apps, platforms, portals,
+              services, and websites. These positions may or may not change in
+              the future without any notification whatsoever. The content on
+              this website might also recommend methods about how to use these
+              software and websites. These methods may or may not be in
+              compliance with the terms of service of the software, apps,
+              platforms, portals, services, and website. India Investments
+              cannot be held liable for any damages or loss anyone suffers
+              because of using the methods shown in our content. The user is
+              advised to read the terms and conditions of the software and
+              websites they use before following the methods and recommendations
+              mentioned in our content.
+            </p>
+            <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">
+              The content on this website may include external 3rd party
+              hyperlinks which we do not own, control, or recommend in any form
+              or manner. Although we have included web archive links for these
+              3rd party hyperlinks wherever possible, the content in the
+              original 3rd party hyperlink may or may not be the same as it was
+              when it was mentioned in our website. India Investments does not
+              bear any responsibility whatsoever about the content on these 3rd
+              party hyperlinks and it cannot be held liable for damages or loss
+              anyone suffers as a result of using these 3rd party hyperlinks.
+            </p>
+            <p className="md:text-xl text-base font-interRegular font-normal text-skin-muted mb-5">
+              The content on this website is provided “as is”, without warranty
+              of any kind, express or implied, including but not limited to the
+              warranties of merchantability, fitness for a particular purpose
+              and non-infringement. In no event shall India Investments and its
+              contributors be liable for any claim, damages or other liability,
+              whether in an action of contract, tort or otherwise, arising from,
+              out of or in connection with the content on this website or the
+              use or other dealings in the content on this website.
+            </p>
+          </div>
+        </div>
+      </section>
+      {/* Content Section End*/}
+    </>
+  );
 }
