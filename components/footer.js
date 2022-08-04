860c33d82553e0e5db470613f27c837d14d828be
zunair <gillanix007@gmail.com>
2022-08-04 08:16:02 -0700

    hero and footer updated

--- components/footer.js
+++ components/footer.js
@@ -1,62 +1,63 @@
 import Link from 'next/link'
-import { FaFacebookSquare, FaInstagramSquare, FaLinkedin, FaMedium, FaTwitterSquare } from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaMediumM, FaTwitter } from 'react-icons/fa'
+import {  GrLinkedinOption } from 'react-icons/gr'
 
 export default function Footer() {
     return (
         <footer>
             <section className="bg-skin-bgFooter">
-            <div className="container mx-auto py-12 grid md:grid-cols-2 grid-cols-1 items-center">
+            <div className="container grid items-center grid-cols-1 py-12 mx-auto md:grid-cols-2">
                 <div className="p-5">
-                    <ul className="flex md:flex-col flex-row gap-5 justify-between">
+                    <ul className="flex flex-row justify-between gap-5 md:flex-col">
                         <li>
                             <Link href="#">
-                                <a className="md:text-lg text-base font-normal text-skin-light font-interRegular">About Us</a>
+                                <a className="text-base font-normal md:text-lg text-skin-light font-interRegular">About Us</a>
                             </Link>
                         </li>
                         <li>
                             <Link href="/disclaimer">
-                                <a className="md:text-lg text-base font-normal text-skin-light font-interRegular">Disclaimer</a>
+                                <a className="text-base font-normal md:text-lg text-skin-light font-interRegular">Disclaimer</a>
                             </Link>
                         </li>
                         <li>
                             <Link href="privacy-policy">
-                                <a className="md:text-lg text-base font-normal text-skin-light font-interRegular">Privacy Policy</a>
+                                <a className="text-base font-normal md:text-lg text-skin-light font-interRegular">Privacy Policy</a>
                             </Link>
                         </li>
                     </ul>
                 </div>
-                <div className="p-5 flex flex-col">
-                    <ul className='flex md:justify-end justify-center mb-2'>
+                <div className="flex flex-col p-5">
+                    <ul className='flex justify-center mb-2 md:justify-end'>
                         <li>
                             <Link href="mailto:support@fundgazer.com">
-                                <a className="md:text-lg text-base font-normal text-skin-light font-interRegular">support@fundgazer.com</a>
+                                <a className="text-base font-normal md:text-lg text-skin-light font-interRegular">support@fundgazer.com</a>
                             </Link>
                         </li>
                     </ul>
-                    <ul className="flex space-x-3 md:justify-end justify-center">
-                        <li>
+                    <ul className="flex justify-center space-x-3 md:justify-end">
+                        <li className='p-[6px] bg-white rounded-lg'>
                             <Link href="#">
-                                <a className="text-4xl text-skin-light"><FaFacebookSquare /></a>
+                                <a className="text-3xl text-[#2B195A]"><FaFacebook size={24}/></a>
                             </Link>
                         </li>
-                        <li>
+                        <li className='p-[6px] bg-white rounded-lg'>
                             <Link href="#">
-                                <a className="text-4xl text-skin-light"><FaTwitterSquare /> </a>
+                                <a className="text-3xl text-[#2B195A]"><FaTwitter size={24}/> </a>
                             </Link>
                         </li>
-                        <li>
+                        <li className='p-[6px] bg-white rounded-lg'>
                             <Link href="#">
-                                <a className="text-4xl text-skin-light"><FaLinkedin /> </a>
+                                <a className="text-3xl text-[#2B195A]"><GrLinkedinOption size={24} /> </a>
                             </Link>
                         </li>
-                        <li>
+                        <li className='p-[6px] bg-white rounded-lg'>
                             <Link href="#">
-                                <a className="text-4xl text-skin-light"><FaInstagramSquare /> </a>
+                                <a className="text-3xl text-[#2B195A]"><FaInstagram size={24}/> </a>
                             </Link>
                         </li>
-                        <li>
+                        <li className='p-[6px] bg-white rounded-lg'>
                             <Link href="#">
-                                <a className="text-4xl text-skin-light"><FaMedium /> </a>
+                                <a className="text-3xl text-[#2B195A]"><FaMediumM size={24}/> </a>
                             </Link>
                         </li>
                     </ul>
@@ -64,9 +65,9 @@ export default function Footer() {
             </div>
             </section>
             <section className='bg-skin-dark'>
-                <div className='container mx-auto py-3'>
+                <div className='container py-3 mx-auto'>
                     <div>
-                        <p className='md:text-base text-sm font-normal text-skin-light text-center font-interRegular'>@Copyrighted by Fundgazer</p>
+                        <p className='text-sm font-normal text-center md:text-base text-skin-light font-interRegular'>@Copyrighted by Fundgazer</p>
                     </div>
                 </div>
             </section>
