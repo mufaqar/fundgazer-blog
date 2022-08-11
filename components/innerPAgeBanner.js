2a8cbba63ba62c58f620a7b4dfefa2f381d0a8f1
Muhammad Zunair <gillanix007@gmail.com>
2022-08-11 16:18:14 +0500

    page top banner

--- components/innerPAgeBanner.js
+++ components/innerPAgeBanner.js
@@ -4,9 +4,9 @@ export default function InnerPAgeBanner() {
     return (
         <>
             {/* Banner Section Start*/}
-            <section className="bg-skin-primary relative py-16">
+            <section className="relative py-16 mt-20 bg-skin-primary">
             <Image src="/images/innerBg.png" alt='innerBg.png' layout="fill" className='object-cover opacity-10'></Image>
-                <div className='container mx-auto py-20 grid md:grid-cols-2 grid-cols-1 gap-8'>
+                <div className='container grid grid-cols-1 gap-8 py-20 mx-auto md:grid-cols-2'>
                 </div>
             </section>
             {/* Banner Section End*/}
