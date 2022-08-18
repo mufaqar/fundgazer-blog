01fdd214682e777b8510073d59c2fb97b23c47f7
Mufaqar Islam <mufaqar@gmail.com>
2022-08-18 20:42:13 +0500

    packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,14 @@
-import Header from "../components/header";
-import "../styles/index.css";
-import { store } from "../lib/store";
-import { Provider } from "react-redux";
+import Header from '../components/header';
+import '../styles/index.css';
 
-import Footer from "../components/footer";
+import Footer from '../components/footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Provider store={store}>
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </Provider>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
