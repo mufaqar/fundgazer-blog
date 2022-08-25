a0e5a37839ac0d2b1ceb8329b23714028fbf8d59
Mufaqar Islam <mufaqar@gmail.com>
2022-08-25 12:10:25 +0500

    Blog header

--- components/header.js
+++ components/header.js
@@ -5,10 +5,9 @@ import Logo from '../public/images/logo.png';
 import whiteLogo from '../public/images/white-logo.png';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import { BiSearch } from "react-icons/bi";
-import Search from '../components/search'
-import SEO from "@bradgarropy/next-seo"
-
+import { BiSearch } from 'react-icons/bi';
+import Search from '../components/search';
+import SEO from '@bradgarropy/next-seo';
 
 function MobileNav({ open, setOpen }) {
   return (
@@ -48,30 +47,35 @@ function MobileNav({ open, setOpen }) {
   );
 }
 
-export default function Header({headerClr}) {
-  
+export default function Header({ headerClr }) {
   const [open, setOpen] = useState(false);
   const router = useRouter();
 
   const SearchBlock = () => {
-    return(
-      <Search />
-    )
-  }
-
- 
+    return <Search />;
+  };
 
   return (
     <>
       <Head>
-        <title>FUNDGAZER - BLOG</title>
+        <title>FUNDGAZER</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta property="og:site_name" content="Fundgazer"></meta>
         <meta property="og:type" content="website"></meta>
       </Head>
-      <SEO title="Fundgazer Blog" description="From building your diversified long-term portfolio and tracking your assets to finding investment ideas, we got you covered." keywords={['latest Blog', 'fundgazer', 'fundgazer news']} />
+      <SEO
+        title="Fundgazer Blog"
+        description="From building your diversified long-term portfolio and tracking your assets to finding investment ideas, we got you covered."
+        keywords={['latest Blog', 'fundgazer', 'fundgazer news']}
+      />
       <nav
-        className={`flex filter px-5 transition-all duration-100 py-4 h-20 items-center shadow-sm fixed top-0 right-0 left-0 z-50 ${router.pathname === '/' ?  headerClr ? 'bg-[#6F49DD]' : 'bg-[#FAF9FD]' : 'bg-[#FAF9FD]'}  
+        className={`flex filter px-5 transition-all duration-100 py-4 h-20 items-center shadow-sm fixed top-0 right-0 left-0 z-50 ${
+          router.pathname === '/'
+            ? headerClr
+              ? 'bg-[#6F49DD]'
+              : 'bg-[#FAF9FD]'
+            : 'bg-[#FAF9FD]'
+        }  
         }`}
       >
         <div className="container flex items-center justify-between mx-auto">
@@ -79,15 +83,24 @@ export default function Header({headerClr}) {
           <div className="flex items-center w-2/3 lg:w-3/12">
             <Link href="/">
               <a className="text-2xl font-semibold">
-                {
-                  router.pathname === '/' ? headerClr ? (
-                  <Image
-                    src={whiteLogo}
-                    alt="logo"
-                    width={165}
-                    height={40}
-                    className="z-10"
-                  ></Image>
+                {router.pathname === '/' ? (
+                  headerClr ? (
+                    <Image
+                      src={whiteLogo}
+                      alt="logo"
+                      width={165}
+                      height={40}
+                      className="z-10"
+                    ></Image>
+                  ) : (
+                    <Image
+                      src={Logo}
+                      alt="logo"
+                      width={165}
+                      height={40}
+                      className="z-10"
+                    ></Image>
+                  )
                 ) : (
                   <Image
                     src={Logo}
@@ -96,21 +109,23 @@ export default function Header({headerClr}) {
                     height={40}
                     className="z-10"
                   ></Image>
-                ) : <Image
-                    src={Logo}
-                    alt="logo"
-                    width={165}
-                    height={40}
-                    className="z-10"
-                  ></Image>
-                }
-                
+                )}
               </a>
             </Link>
           </div>
           <div className="flex items-center justify-end w-9/12">
-            <div className={`md:hidden mr-5 ${ router.pathname === '/blog' ? 'block' : 'hidden'}`}> {/* search icon */}
-              <BiSearch className={`text-3xl ${headerClr ? 'text-white' : 'text-black'} `}  />
+            <div
+              className={`md:hidden mr-5 ${
+                router.pathname === '/blog' ? 'block' : 'hidden'
+              }`}
+            >
+              {' '}
+              {/* search icon */}
+              <BiSearch
+                className={`text-3xl ${
+                  headerClr ? 'text-white' : 'text-black'
+                } `}
+              />
             </div>
             <div
               className="relative z-50 flex flex-col items-center justify-between h-6 w-7 md:hidden"
@@ -120,39 +135,76 @@ export default function Header({headerClr}) {
             >
               {/* hamburger button */}
               <span
-                className={`h-1 w-full ${headerClr ? 'bg-white' : 'bg-black'}  rounded-lg transform transition duration-300 ease-in-out ${
+                className={`h-1 w-full ${
+                  headerClr ? 'bg-white' : 'bg-black'
+                }  rounded-lg transform transition duration-300 ease-in-out ${
                   open ? 'rotate-45 translate-y-3.5' : ''
                 }`}
               />
               <span
-                className={`h-1 w-full ${headerClr ? 'bg-white' : 'bg-black'} rounded-lg transition-all duration-300 ease-in-out ${
+                className={`h-1 w-full ${
+                  headerClr ? 'bg-white' : 'bg-black'
+                } rounded-lg transition-all duration-300 ease-in-out ${
                   open ? 'w-0' : 'w-full'
                 }`}
               />
               <span
-                className={`h-1 w-full ${headerClr ? 'bg-white' : 'bg-black'} rounded-lg transform transition duration-300 ease-in-out ${
+                className={`h-1 w-full ${
+                  headerClr ? 'bg-white' : 'bg-black'
+                } rounded-lg transform transition duration-300 ease-in-out ${
                   open ? '-rotate-45 -translate-y-3.5' : ''
                 }`}
               />
             </div>
 
             <div
-              className={`hidden text-xl font-bold md:flex space-x-10 ${ router.pathname === '/' ? headerClr ? 'text-[#FAF9FD]' : 'text-black' : 'text-black' }`}
+              className={`hidden text-xl font-bold md:flex space-x-10 ${
+                router.pathname === '/'
+                  ? headerClr
+                    ? 'text-[#FAF9FD]'
+                    : 'text-black'
+                  : 'text-black'
+              }`}
             >
-              <Link href="/our-community" className={`mx-4 font-interBold tracking-wider `} >
-                <a className={`${router.pathname === '/' ?  headerClr ? 'hover:text-black' : 'hover:text-[#6F49DD]' : 'hover:text-[#6F49DD]'} `} onClick={()=>setOpen(false)}>Join Our Community</a>
+              <Link
+                href="/our-community"
+                className={`mx-4 font-interBold tracking-wider `}
+              >
+                <a
+                  className={`${
+                    router.pathname === '/'
+                      ? headerClr
+                        ? 'hover:text-black'
+                        : 'hover:text-[#6F49DD]'
+                      : 'hover:text-[#6F49DD]'
+                  } `}
+                  onClick={() => setOpen(false)}
+                >
+                  Join Our Community
+                </a>
               </Link>
-              <Link href="/blog" className={`mx-4 font-interBold tracking-wider  `} >
-                <a className={`${router.pathname === '/' ?  headerClr ? 'hover:text-black' : 'hover:text-[#6F49DD]' : 'hover:text-[#6F49DD]'} `} onClick={()=>setOpen(false)} >Blogs</a>
+              <Link
+                href="/blog"
+                className={`mx-4 font-interBold tracking-wider  `}
+              >
+                <a
+                  className={`${
+                    router.pathname === '/'
+                      ? headerClr
+                        ? 'hover:text-black'
+                        : 'hover:text-[#6F49DD]'
+                      : 'hover:text-[#6F49DD]'
+                  } `}
+                  onClick={() => setOpen(false)}
+                >
+                  Blogs
+                </a>
               </Link>
             </div>
-            
           </div>
         </div>
-       {/* <SearchBlock/>  */}
+        {/* <SearchBlock/>  */}
       </nav>
     </>
   );
 }
-
-
