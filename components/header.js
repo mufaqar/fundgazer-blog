fd45a1959e4fa16fc8f709745f76fa7881b2ce58
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-10-16 13:39:00 +0500

    Header

--- components/header.js
+++ components/header.js
@@ -7,24 +7,24 @@ import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { BiSearch } from 'react-icons/bi';
 import Search from '../components/search';
+import {AiFillCaretDown} from 'react-icons/ai'
 
 
 
 
 function MobileNav({ open, setOpen }) {
-  
-  
+
+
   return (
     <div
-      className={`absolute top-0 left-0 h-screen w-screen bg-[#FAF9FD] transform ${
-        open ? '-translate-x-0' : '-translate-x-full'
-      } transition-transform duration-300 ease-in-out filter drop-shadow-md h-80`}
+      className={`absolute top-0 left-0 h-screen w-screen bg-[#FAF9FD] transform ${open ? '-translate-x-0' : '-translate-x-full'
+        } transition-transform duration-300 ease-in-out filter drop-shadow-md h-80`}
     >
       <div className="flex items-center justify-center filter bg-[#FAF9FD] h-20">
         {/*logo container*/}
       </div>
       {/* <div className={`flex space-y-2 flex-col ml-4 ${open ? 'mt-8' : 'mt-0'}`}> */}
-        {/* <Link
+      {/* <Link
           className="my-4 text-xl font-bold "
           href="/our-community"
           onClick={() =>
@@ -35,7 +35,7 @@ function MobileNav({ open, setOpen }) {
         >
           Join Our Community
         </Link> */}
-        {/* <Link
+      {/* <Link
           className="my-4 text-xl font-bold"
           href="/blog"
           onClick={() =>
@@ -46,7 +46,7 @@ function MobileNav({ open, setOpen }) {
         >
           Blogs
         </Link> */}
-      </div>
+    </div>
     // </div>
   );
 }
@@ -55,7 +55,7 @@ export default function Header({ headerClr, serachInput, setSearchInput }) {
   const [open, setOpen] = useState(false);
   const router = useRouter();
   const [handleMobileSearch, setHandleMobileSearch] = useState(false)
-  
+
   return (
     <>
       <Head>
@@ -64,40 +64,30 @@ export default function Header({ headerClr, serachInput, setSearchInput }) {
         <meta property="og:site_name" content="Fundgazer"></meta>
         <meta property="og:type" content="website"></meta>
       </Head>
-      
+
       <nav
-        className={`flex filter px-5 transition-all duration-100 py-4 h-20 items-center shadow-sm fixed top-0 right-0 left-0 z-50 ${
-          router.pathname === '/'
-            ? headerClr
-              ? 'bg-[#6F49DD]'
-              : 'bg-[#FAF9FD]'
+        className={`flex filter px-5 transition-all duration-100 py-4 h-20 items-center shadow-sm fixed top-0 right-0 left-0 z-50 ${router.pathname === '/'
+          ? headerClr
+            ? 'bg-[#6F49DD]'
             : 'bg-[#FAF9FD]'
-        }  
+          : 'bg-[#FAF9FD]'
+          }  
         }`}
       >
         <div className="container flex items-center justify-between mx-auto">
           <MobileNav open={open} setOpen={setOpen} />
           <div className="flex items-center w-2/3 lg:w-3/12">
             <Link href="/" className="text-2xl font-semibold">
-           
-                {router.pathname === '/' ? (
-                  headerClr ? (
-                    <Image
-                      src={whiteLogo}
-                      alt="logo"
-                      width={165}
-                      height={40}
-                      className="z-10"
-                    ></Image>
-                  ) : (
-                    <Image
-                      src={Logo}
-                      alt="logo"
-                      width={165}
-                      height={40}
-                      className="z-10"
-                    ></Image>
-                  )
+
+              {router.pathname === '/' ? (
+                headerClr ? (
+                  <Image
+                    src={whiteLogo}
+                    alt="logo"
+                    width={165}
+                    height={40}
+                    className="z-10"
+                  ></Image>
                 ) : (
                   <Image
                     src={Logo}
@@ -106,23 +96,30 @@ export default function Header({ headerClr, serachInput, setSearchInput }) {
                     height={40}
                     className="z-10"
                   ></Image>
-                )}
-         
+                )
+              ) : (
+                <Image
+                  src={Logo}
+                  alt="logo"
+                  width={165}
+                  height={40}
+                  className="z-10"
+                ></Image>
+              )}
+
             </Link>
           </div>
-          <div className="flex items-center justify-end w-9/12">
+          <div className="flex items-center lg:justify-start justify-end w-1/3 lg:w-6/12">
             <div
-              className={`md:hidden mr-5 ${
-                router.pathname === '/blog' ? 'block' : 'hidden'
-              }`}
+              className={`md:hidden mr-5 ${router.pathname === '/blog' ? 'block' : 'hidden'
+                }`}
             >
               {' '}
               {/* search icon */}
               <BiSearch
-                className={`text-3xl ${
-                  headerClr ? router.pathname === '/' ? 'text-white' : 'text-black' : 'text-black'
-                } ${handleMobileSearch ? 'text-[#6F49DD]' : ''}`}
-                onClick={()=>setHandleMobileSearch(!handleMobileSearch)}
+                className={`text-3xl ${headerClr ? router.pathname === '/' ? 'text-white' : 'text-black' : 'text-black'
+                  } ${handleMobileSearch ? 'text-[#6F49DD]' : ''}`}
+                onClick={() => setHandleMobileSearch(!handleMobileSearch)}
               />
             </div>
             <div
@@ -133,79 +130,140 @@ export default function Header({ headerClr, serachInput, setSearchInput }) {
             >
               {/* hamburger button */}
               <span
-                className={`h-1 w-full ${
-                  headerClr ? router.pathname === '/' ? 'bg-white' : 'bg-black' : 'bg-black'
-                }  rounded-lg transform transition duration-300 ease-in-out ${
-                  open ? 'rotate-45 translate-y-3.5' : ''
-                }`}
+                className={`h-1 w-full ${headerClr ? router.pathname === '/' ? 'bg-white' : 'bg-black' : 'bg-black'
+                  }  rounded-lg transform transition duration-300 ease-in-out ${open ? 'rotate-45 translate-y-3.5' : ''
+                  }`}
               />
               <span
-                className={`h-1 w-full ${
-                  headerClr ? router.pathname === '/' ? 'bg-white' : 'bg-black' : 'bg-black'
-                } rounded-lg transition-all duration-300 ease-in-out ${
-                  open ? 'w-0' : 'w-full'
-                }`}
+                className={`h-1 w-full ${headerClr ? router.pathname === '/' ? 'bg-white' : 'bg-black' : 'bg-black'
+                  } rounded-lg transition-all duration-300 ease-in-out ${open ? 'w-0' : 'w-full'
+                  }`}
               />
               <span
-                className={`h-1 w-full ${
-                  headerClr ? router.pathname === '/' ? 'bg-white' : 'bg-black' : 'bg-black'
-                } rounded-lg transform transition duration-300 ease-in-out ${
-                  open ? '-rotate-45 -translate-y-3.5' : ''
-                }`}
+                className={`h-1 w-full ${headerClr ? router.pathname === '/' ? 'bg-white' : 'bg-black' : 'bg-black'
+                  } rounded-lg transform transition duration-300 ease-in-out ${open ? '-rotate-45 -translate-y-3.5' : ''
+                  }`}
               />
             </div>
-
             <div
-              className={`hidden text-xl font-bold md:flex space-x-10 ${
-                router.pathname === '/'
-                  ? headerClr
-                    ? 'text-[#FAF9FD]'
-                    : 'text-black'
+              className={`hidden text-xl font-bold md:flex gap-10 ${router.pathname === '/'
+                ? headerClr
+                  ? 'text-[#FAF9FD]'
                   : 'text-black'
-              }`}
+                : 'text-black'
+                }`}
             >
-              {/* <Link
-                href="/our-community"
-                className={`mx-4 font-interBold tracking-wider `}
+              <Link
+                href="/products"
+                className={`font-interBold tracking-wider  `}
               >
-                <a
-                  className={`${
-                    router.pathname === "/our-community" ? headerClr ? 'hover:text-black ' : 'hover:text-[#6F49DD] text-[#6F49DD]' : headerClr ? 'hover:underline' : 'hover:text-[#6F49DD]'
-                  } `}
+                <span
+                  className={`flex gap-2 items-center ${router.pathname === "/products"
+                    ? headerClr
+                      ? 'hover:text-black'
+                      : 'hover:text-[#6F49DD] text-[#6F49DD]'
+                    : headerClr ? 'hover:underline' : 'hover:text-[#6F49DD]'
+                    } `}
+                  onClick={() => setOpen(false)}
+                >
+                  Products <AiFillCaretDown />
+                </span>
+              </Link>
+              <Link
+                href="/resources"
+                className={`font-interBold tracking-wider  `}
+              >
+                <span
+                  className={`flex gap-2 items-center ${router.pathname === "/resources"
+                    ? headerClr
+                      ? 'hover:text-black'
+                      : 'hover:text-[#6F49DD] text-[#6F49DD]'
+                    : headerClr ? 'hover:underline' : 'hover:text-[#6F49DD]'
+                    } `}
+                  onClick={() => setOpen(false)}
+                >
+                  Resources <AiFillCaretDown />
+                </span>
+              </Link>
+              <Link
+                href="/our-community"
+                className={`font-interBold tracking-wider `}
+              >
+                <span
+                  className={`${router.pathname === "/our-community" ? headerClr ? 'hover:text-black ' : 'hover:text-[#6F49DD] text-[#6F49DD]' : headerClr ? 'hover:underline' : 'hover:text-[#6F49DD]'
+                    } `}
                   onClick={() => setOpen(false)}
                 >
                   Join Our Community
-                </a>
-              </Link> */}
-              {/* <Link
-                href="/blog"
-                className={`mx-4 font-interBold tracking-wider  `}
+                </span>
+              </Link>
+            </div>
+          </div>
+          <div className="lg:flex hidden items-center justify-end w-full lg:w-3/12">
+            <div
+              className={`text-lg font-bold flex gap-7 items-center ${router.pathname === '/'
+                ? headerClr
+                  ? 'text-[#FAF9FD]'
+                  : 'text-black'
+                : 'text-black'
+                }`}
+            >
+              <Link
+                href="/download-app"
+                className={`font-interBold tracking-wider `}
               >
-                <a
-                  className={`${
-                    router.pathname === "/blog"
-                      ? headerClr
-                        ? 'hover:text-black'
-                        : 'hover:text-[#6F49DD] text-[#6F49DD]'
-                      : headerClr ? 'hover:underline' : 'hover:text-[#6F49DD]'
-                  } `}
+                <span
+                  className={`${router.pathname === "/download-app" ? headerClr ? 'hover:text-black ' : 'hover:text-[#6F49DD] text-[#6F49DD]' : headerClr ? 'hover:underline' : 'hover:text-[#6F49DD]'
+                    } `}
                   onClick={() => setOpen(false)}
                 >
-                  Blogs
-                </a>
-              </Link> */}
+                  Download The App
+                </span>
+              </Link>
+              <Link
+                href="/play-store"
+                className={`font-interBold tracking-wider  `}
+              >
+                <span
+                  className={`${router.pathname === "/play-store"
+                    ? headerClr
+                      ? 'hover:text-black'
+                      : 'hover:text-[#6F49DD] text-[#6F49DD]'
+                    : headerClr ? 'hover:underline' : 'hover:text-[#6F49DD]'
+                    } `}
+                  onClick={() => setOpen(false)}
+                >
+                  <Image src="/images/play-store.png" alt="/images/play-store.png" width={42} height={43} />
+                </span>
+              </Link>
+              <Link
+                href="/apple"
+                className={`font-interBold tracking-wider  `}
+              >
+                <span
+                  className={`${router.pathname === "/apple"
+                    ? headerClr
+                      ? 'hover:text-black'
+                      : 'hover:text-[#6F49DD] text-[#6F49DD]'
+                    : headerClr ? 'hover:underline' : 'hover:text-[#6F49DD]'
+                    } `}
+                  onClick={() => setOpen(false)}
+                >
+                  <Image src="/images/apple.png" alt="/images/apple.png" width={39} height={43} />
+                </span>
+              </Link>
             </div>
           </div>
         </div>
       </nav>
 
       <section className={`fixed bg-white z-10 w-full md:hidden top-20 ${handleMobileSearch ? 'block' : 'hidden'}`}>
-        <Search 
+        <Search
           setSearchInput={setSearchInput}
           serachInput={serachInput}
         />
       </section>
-      
+
     </>
   );
 }
