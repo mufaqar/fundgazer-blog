0762c8396ce113bfc0af511130d77749e8f24e14
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-10-23 15:59:47 +0500

    Spromma

---
+++ components/statics.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Statics() {
+    return (
+        <div className='bg-[#F2EDFF] max-w-[1137px] mx-auto p-5 rounded-[5px]'>
+            <div>
+                <div>
+                    <h3 className='text-4xl leading-5 font-semibold font-interMedium'>
+                        Statistics
+                    </h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Statics
---
+++ pages/spromma.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Spromma() {
+    return (
+        <main>
+            <section className='md:pt-40 pt-32 pb-20 px-5 bg-[#F7F7F9]'>
+                <div className='container mx-auto'>
+                    <h2 className='md:text-[48px] text-2xl leading-[64px] font-semibold font-interRegular text-skin-primary'>
+                        Spromma
+                    </h2>
+                    <div className='grid md:grid-cols-3 grid-cols-1 md:gap-16 gap-0 mt-5'>
+                        <p className='md:text-[22px] md:leading-[36.4px] text-sm font-normal font-interRegular max-w-[392px]'>
+                            Every strategy on Composer is fully
+                            editable. Swap out assets, adjust
+                            programmatic logic, and tweak
+                            parameters.
+                        </p>
+                        <p className='md:text-[22px] md:leading-[38.4px] text-sm font-normal font-interRegular max-w-[392px]'>
+                            Use our no-code, visual editor to modify
+                            symphonies or create your own from
+                            scratch.
+                        </p>
+                        <p className='md:text-[22px] md:leading-[36.4px] text-sm font-normal font-interRegular max-w-[392px]'>
+                            Every strategy on Composer is fully
+                            editable. Swap out assets, adjust
+                            programmatic logic, and tweak
+                            parameters.
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
