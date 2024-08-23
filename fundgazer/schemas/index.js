8bc780ffbaa5abbbd464689279bd7f6c78f473f7
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2024-08-23 19:37:35 +0500

    updated

--- fundgazer/schemas/index.js
+++ fundgazer/schemas/index.js
@@ -1,12 +1,10 @@
 import { author } from './author'
 import { blog } from './blog'
 import { comment } from './comment'
-import {eventType} from './eventTypes'
 import { tags } from './tags'
 import { team } from './team'
 
 export const schemaTypes = [
-    eventType,
     blog,
     author,
     tags,
